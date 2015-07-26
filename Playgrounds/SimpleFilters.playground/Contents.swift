471d635c3d08a7e1e78254c624a8bbadceb8afd8
Jonathan Wight <jwight@mac.com>
2015-07-26 09:35:47 -0700

    Added docs to Filter playground.

--- Playgrounds/SimpleFilters.playground/Contents.swift
+++ Playgrounds/SimpleFilters.playground/Contents.swift
@@ -1,14 +1,21 @@
 //: Playground - noun: a place where people can play
-
 import Cocoa
 import CoreImage
 import SwiftGraphics
 
-let f1 = SunbeamsGenerator(inputSunRadius:CGFloat(200))
+//: CoreImage filters are exposed as unique classes.
+//: Unique filter classes are subclasses of the "Filter" class. Filter is _not_ a subclass of CIFilter.
+//: A newly created instance will automatically have setDefaults() called on it.
+let f1 = SunbeamsGenerator()
+
+//: All CoreImage attributes are exposed as Swift properties
+f1.inputSunRadius = 200
 f1.outputImage
 
+//: You can even define some or all initial values on filter creation.
 let f2 = Crystallize(inputImage:f1.outputImage, inputRadius:50)
 f2.outputImage
+
 let f3 = TwirlDistortion(inputImage: f2.outputImage)
 f3.inputRadius = 300
 f3.outputImage
