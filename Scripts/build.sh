15391f2d2842ec24b566851957060e8e37331d67
Jonathan Wight <jwight@mac.com>
2015-08-15 13:39:16 -0700

    Updated scripts/build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-xcodebuild -version | grep "Xcode 7." > /dev/null || { echo 'Not running Xcode 7' ; exit 1; }
+xcodebuild -version | grep "Xcode 7" > /dev/null || { echo 'Not running Xcode 7' ; exit 1; }
 
 cd `git rev-parse --show-toplevel`
 
-xctool -project SwiftGraphics.xcodeproj -scheme SwiftGraphics_OSX build test || exit $!
-#xctool -project SwiftGraphics.xcodeproj -scheme SwiftGraphics_OSX_Playground build test  || exit $!
-#xctool -project SwiftGraphics.xcodeproj -scheme SwiftGraphics_OSX_Scratch build test
-#xctool -project SwiftGraphics.xcodeproj -scheme SwiftGraphics_OSX_UITest build test
-#xctool -project SwiftGraphics.xcodeproj -scheme SwiftGraphics_iOS -sdk iphonesimulator build
+xctool -project SwiftGraphics.xcodeproj -scheme All build test || exit $!
